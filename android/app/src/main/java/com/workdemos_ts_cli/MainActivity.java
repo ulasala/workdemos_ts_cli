e6e11c46d3ab028cc5978acaaa8517a6559e44cf
ulasala <61379162+ulasala@users.noreply.github.com>
2021-08-25 15:10:38 +0530

    navigation_package_setup_work

--- android/app/src/main/java/com/workdemos_ts_cli/MainActivity.java
+++ android/app/src/main/java/com/workdemos_ts_cli/MainActivity.java
@@ -2,6 +2,8 @@ package com.workdemos_ts_cli;
 
 import com.facebook.react.ReactActivity;
 
+import android.os.Bundle;
+
 public class MainActivity extends ReactActivity {
 
   /**
@@ -11,5 +13,9 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "workdemos_ts_cli";
+  }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(null);
   }
 }
